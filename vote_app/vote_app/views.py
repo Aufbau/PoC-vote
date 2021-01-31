2700d56905c18317e1db5250419d0d3e101b7464
abhinavr4 <abhinavrituraj4@gmail.com>
2021-01-31 22:03:59 +0530

    redis added

--- vote_app/vote_app/views.py
+++ vote_app/vote_app/views.py
@@ -1,10 +1,16 @@
-from rest_framework.views import APIView
+import random
 from django.shortcuts import render
+from rest_framework.views import APIView
+from redis import Redis
+import json
 
 class VoteApp(APIView):
-	def get(self, request):
-		return render(request, 'voter_page.html')
+    def get(self, request):
+        return render(request, 'voter_page.html')
 
-	def post(self, request):
-		voted_fruit = request.data['fruit']
-		return render(request, 'voted_page.html', {'data': voted_fruit})
+    def post(self, request):
+        r = Redis(host="super-redis", port=6379)
+        voted_fruit = request.data['fruit']
+        data = json.dumps({'client_id': str(random.randint(100, 1000000)), 'vote_option': voted_fruit})
+        r.rpush('votes', data)
+        return render(request, 'voted_page.html', {'data': voted_fruit})
