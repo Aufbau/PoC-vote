00496398c76f66c0312087bbca5565039e606a65
abhinavr4 <abhinavrituraj4@gmail.com>
2021-02-03 19:29:10 +0530

    fixed injection issue, added dynamic html, form

---
+++ vote_app/vote_app/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+CHOICES = [
+    ('Apple', 'Apple'), ('Mango', 'Mango')
+]
+
+class VoteForm(forms.Form):
+    
+    fruit = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+
+    def validate_input(self, fruit):
+        for fruits in CHOICES:
+            if fruit in fruits[0]:
+                return True
+        return False
--- vote_app/vote_app/views.py
+++ vote_app/vote_app/views.py
@@ -4,27 +4,38 @@ from rest_framework.views import APIView
 from redis import Redis
 import json
 import logging
+from .forms import VoteForm
 
 logging.basicConfig(level=logging.INFO)
 
 r = Redis(host="super-redis", port=6379)
 logging.info("Vote app connected to super-redis ...")
 
+form = VoteForm()
+
 class VoteApp(APIView):
 
     def get(self, request):
-        if request.session.session_key is not None:
-            msg = 'It appears you have already voted for ' + str(request.session['vote_option']) + '! Changed your mind?'
-            logging.info('session_key: {}'.format(request.session.session_key))
-            return render(request, 'voter_page.html', {'data': msg})
-        return render(request, 'voter_page.html')
 
-    def post(self, request):
         if request.session.session_key is None:
+            return render(request, 'voter_page.html', {'form': form})
+            
+        logging.info('session_key: {}'.format(request.session.session_key))
+        return render(request, 'voter_page.html', {'data': request.session['vote_option'],'form': form})
+     
+    def post(self, request):
+
+        voted_fruit = request.data['fruit']
+        validity = form.validate_input(voted_fruit)
+
+        if request.session.session_key is None and validity:
             request.session.create()
             logging.info('session_key: {}'.format(request.session.session_key))
-        voted_fruit = request.data['fruit']
-        request.session['vote_option'] = voted_fruit
-        data = json.dumps({'client_id': request.session.session_key, 'vote_option': voted_fruit})
-        r.rpush('votes', data)
-        return render(request, 'voted_page.html', {'data': voted_fruit})
+        
+        if validity:
+            request.session['vote_option'] = voted_fruit
+            data = json.dumps({'client_id': request.session.session_key, 'vote_option': voted_fruit})
+            r.rpush('votes', data)
+            return render(request, 'voted_page.html', {'data': voted_fruit})
+
+        return render(request, 'voted_page.html')
