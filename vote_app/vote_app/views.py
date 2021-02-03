a3cd071c1e463611f111aafed44c7d108b29afbb
abhinavr4 <abhinavrituraj4@gmail.com>
2021-02-03 11:40:11 +0530

    vote choice shown, db file removed and ignored

--- vote_app/vote_app/views.py
+++ vote_app/vote_app/views.py
@@ -8,13 +8,13 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 r = Redis(host="super-redis", port=6379)
-logging.info("Vote app onnected to super-redis ...")
+logging.info("Vote app connected to super-redis ...")
 
 class VoteApp(APIView):
 
     def get(self, request):
         if request.session.session_key is not None:
-            msg = 'It appears you have already voted! Changed your mind?'
+            msg = 'It appears you have already voted for ' + str(request.session['vote_option']) + '! Changed your mind?'
             logging.info('session_key: {}'.format(request.session.session_key))
             return render(request, 'voter_page.html', {'data': msg})
         return render(request, 'voter_page.html')
@@ -24,6 +24,7 @@ class VoteApp(APIView):
             request.session.create()
             logging.info('session_key: {}'.format(request.session.session_key))
         voted_fruit = request.data['fruit']
+        request.session['vote_option'] = voted_fruit
         data = json.dumps({'client_id': request.session.session_key, 'vote_option': voted_fruit})
         r.rpush('votes', data)
         return render(request, 'voted_page.html', {'data': voted_fruit})
